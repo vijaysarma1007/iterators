0405f0cf1df3fe2a5fc0ed3abf77935767b65224
vijaysarma1007 <svmsarma53@gmail.com>
2026-04-15 22:35:06 +0530

    added iteratir and next methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let numbers = vec![4, 8, 15, 16];
+    let mut my_iterator = numbers.into_iter();
+
+    println!("{:?}", my_iterator.next());
+    println!("{:?}", my_iterator);
+
+    println!("{:?}", my_iterator.next());
+    println!("{:?}", my_iterator);
+    
+    println!("{:?}", my_iterator.next());
+    println!("{:?}", my_iterator);
+
+    println!("{:?}", my_iterator.next());
+    println!("{:?}", my_iterator);
+
+    println!("{:?}", my_iterator.next());
+    println!("{:?}", my_iterator);
+
+
+    let mut my_hasmap = HashMap::new();
+    my_hasmap.insert("CBS", 2);
+    my_hasmap.insert("CBS2", 3);
+    let hashmap_iter = my_hasmap.into_iter();
+
+    for hash in hashmap_iter {
+        println!("{:?}", hash);
+    }
 }
